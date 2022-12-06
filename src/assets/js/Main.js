e174bc934b64fa82d5c6ca00e8beee269751a88e
kimsangjunv1 <ingyoe64@gmail.com>
2022-12-06 19:21:49 +0900

    2022.12.06

---
+++ src/assets/js/Main.js
@@ -0,0 +1,310 @@
+// 레니즈 스크롤
+
+// const lenis = new Lenis({
+//   duration: 2,
+//   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+//   direction: "vertical", // vertical, horizontal
+//   gestureDirection: "vertical", // vertical, horizontal, both
+//   smooth: true,
+//   // mouseMultiplier: 1,
+//   // smoothTouch: false,
+//   // touchMultiplier: 2,
+//   // infinite: false,
+// });
+
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+
+// requestAnimationFrame(raf);
+
+//   가로 스크롤 모음
+
+gsap.registerPlugin(ScrollTrigger);
+
+// JS 이펙트 설명
+let sections = gsap.utils.toArray(".section3_item");
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal",
+    markers: true,
+    pin: true,
+    scrub: true,
+    // snap: 1 / (sections.length - 1),
+    end: "+=5000",
+    // end: () => "+=" + document.querySelector(".horizontal"),
+  },
+});
+
+// 게임 이펙트
+let sections2 = gsap.utils.toArray(".panel2");
+gsap.to(sections2, {
+  xPercent: -100 * (sections2.length - 1),
+  ease: "none",
+  // duration: 122.5,
+  scrollTrigger: {
+    trigger: ".horizontal2",
+    markers: true,
+    pin: true,
+    scrub: 1,
+    // snap: 1 / (sections2.length - 1),
+    end: "+=11000",
+  },
+});
+
+// ScrollTrigger.create({
+//   // 어디에 닿으면 반응할건지
+//   trigger: ".horizontal2",
+//   scrub: 1,
+//   // 어디다가 적용할건지
+//   animation: gsap.fromTo(
+//     ".section3_item_bottom",
+//     {
+//       transform: "translate(-50%,0) rotate(0deg)",
+//     },
+//     {
+//       transform: "translate(-60%,1020%) scale(3.0) rotate(200deg)",
+//       // filter: "blur(3px)",
+//     }
+//   ),
+// });
+
+//   상단바
+
+// 상단
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header",
+    {
+      color: "#fff",
+      background: "linear-gradient(180deg, #5046ac, #5046ac00)",
+    },
+    {
+      color: "#fff",
+      background: "linear-gradient(180deg, #5046ac, #5046ac00)",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".editorial_two",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header a",
+    {
+      color: "#fff",
+    },
+    {
+      color: "#fff",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".editorial_two",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".porfolio_desc_fixed p",
+    {
+      color: "#fff",
+    },
+    {
+      color: "#fff",
+    }
+  ),
+});
+
+// 중간
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".editorial_two",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header",
+    {
+      color: "#000",
+      background: "linear-gradient(180deg, #5046ac, #5046ac00)",
+    },
+    {
+      color: "#000",
+      background: "linear-gradient(180deg, #ededf4, #ededf400)",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".editorial_two",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header a",
+    {
+      color: "#000",
+    },
+    {
+      color: "#000",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".editorial_two",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".porfolio_desc_fixed p",
+    {
+      color: "#000",
+    },
+    {
+      color: "#000",
+    }
+  ),
+});
+// ScrollTrigger.create({
+//   // 어디에 닿으면 반응할건지
+//   trigger: ".editorial_two",
+//   scrub: 1,
+//   // 어디다가 적용할건지
+//   animation: gsap.fromTo(
+//     "#header",
+//     {
+//       color: "#fff",
+//       background: "linear-gradient(#5046ac, #5046ac00)",
+//     },
+//     {
+//       color: "#353541",
+//       background: "linear-gradient(#f6f6fb, #f6f6fb00)",
+//     }
+//   ),
+//   animation: gsap.fromTo(
+//     "#header a",
+//     {
+//       color: "#fff",
+//     },
+//     {
+//       color: "#353541",
+//     }
+//   ),
+//   animation: gsap.fromTo(
+//     ".porfolio_desc_fixed p",
+//     {
+//       color: "#fff",
+//     },
+//     {
+//       color: "#353541",
+//     }
+//   ),
+// });
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".horizontal3",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header",
+    {
+      color: "#000",
+      background: "linear-gradient(180deg, #f6f6fb, #f6f6fb00)",
+    },
+    {
+      color: "#fff",
+      background: "linear-gradient(180deg, #100f1c6b, #100f1c00)",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".horizontal3",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header a",
+    {
+      color: "#000",
+    },
+    {
+      color: "#fff",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".horizontal3",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".porfolio_desc_fixed p",
+    {
+      color: "#000",
+    },
+    {
+      color: "#fff",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section15_flex_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header",
+    {
+      background: "linear-gradient(180deg, #100f1c6b, #100f1c00)",
+      color: "#fff",
+    },
+    {
+      background: "linear-gradient(180deg, #f6f6fb, #f6f6fb00)",
+      color: "#000",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section15_flex_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    "#header a",
+    {
+      color: "#ffffff8c",
+    },
+    {
+      color: "#000",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section15_flex_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".porfolio_desc_fixed p",
+    {
+      color: "#fff",
+    },
+    {
+      color: "#000",
+    }
+  ),
+});
---
+++ src/assets/js/Section.js
@@ -0,0 +1,737 @@
+// 메인 먼지
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_01",
+    {
+      transform: "translate(-50%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(-60%,1020%) scale(3.0) rotate(200deg)",
+      // filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_02",
+    {
+      transform: "translate(-60%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(-500%,-120%) scale(4.0) rotate(200deg)",
+      filter: "blur(6px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_03",
+    {
+      transform: "translate(70%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(300%,-200%) scale(1.5) rotate(200deg)",
+      filter: "blur(5px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_07",
+    {
+      transform: "translate(70%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(300%,-50%) scale(1.5) rotate(200deg)",
+      filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_08",
+    {
+      transform: "translate(-60%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(-500%,-120%) scale(2.0) rotate(200deg)",
+      filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_09",
+    {
+      transform: "translate(70%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(300%,-50%) scale(1.5) rotate(200deg)",
+      filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_10",
+    {
+      transform: "translate(-60%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(-500%,-120%) scale(2.0) rotate(200deg)",
+      filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".dust_11",
+    {
+      transform: "translate(-60%,0) rotate(0deg)",
+    },
+    {
+      transform: "translate(-500%,-120%) scale(2.0) rotate(200deg)",
+      filter: "blur(3px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".section1_desc h2",
+    {
+      height: "27.2vw",
+    },
+    {
+      height: "0vw",
+      // filter: "blur(3px)",
+    }
+  ),
+});
+
+//   섹션 2
+ScrollTrigger.create({
+  // 어디에 닿으면 반응할건지
+  trigger: ".section2_cont",
+  scrub: 1,
+  // 어디다가 적용할건지
+  animation: gsap.fromTo(
+    ".section .lama",
+    {
+      right: "-300px",
+    },
+    {
+      right: "0px",
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n1",
+    {
+      scale: (1.4, 1.4),
+      opacity: 0,
+      transformOrigin: "1300px 18px",
+      transform: "translate(0,50px)",
+      filter: "blur(15px)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-30px,0)",
+      filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n2",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "900px 18px",
+      transform: "translate(0,50px)",
+      filter: "blur(7px)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n3",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "500px 18px",
+      transform: "translate(0,50px)",
+      filter: "blur(2px)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n4",
+    {
+      scale: (1.3, 1.3),
+      // opacity: 0,
+      transformOrigin: "500px 18px",
+      transform: "translate(0,50px)",
+      // filter: "blur(4px)",
+    },
+    {
+      scale: 1,
+      // opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      // filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n5",
+    {
+      scale: (1.3, 1.3),
+      // opacity: 0,
+      transformOrigin: "500px 18px",
+      transform: "translate(0,50px)",
+      // filter: "blur(4px)",
+    },
+    {
+      scale: 1,
+      // opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      // filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n6",
+    {
+      scale: (1.3, 1.3),
+      // opacity: 0,
+      transformOrigin: "500px 18px",
+      transform: "translate(0,50px)",
+      // filter: "blur(4px)",
+    },
+    {
+      scale: 1,
+      // opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      // filter: "blur(0px)",
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".section2_cont",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".n7",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "500px 18px",
+      transform: "translate(0,50px)",
+      filter: "blur(4px)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+      filter: "blur(0px)",
+    }
+  ),
+});
+
+// 섹션 3_bottom
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".section3_item_bottom", {
+    transform: "translatex(0%)",
+  }),
+  animation: gsap.to(".section3_item_bottom", {
+    transform: "translatex(-50%)",
+  }),
+});
+// 섹션 3_bottom
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".section3_item_bottom2", {
+    transform: "translatex(0%) scalex(-1)",
+  }),
+  animation: gsap.to(".section3_item_bottom2", {
+    transform: "translatex(-30%) scalex(-1)",
+  }),
+});
+
+// 섹션 3_가운데 글씨
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".section3_fixed", {
+    //   transform: "translatex(0%) scalex(-1)",
+    opacity: 1,
+  }),
+  animation: gsap.to(".section3_fixed", {
+    //   transform: "translatex(-30%) scalex(-1)",
+    opacity: 0.3,
+  }),
+});
+
+ScrollTrigger.create({
+  trigger: ".section3_fixed",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".section3_fixed",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "900px 18px",
+      transform: "translate(-50%, -50%)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate(-50%, -50%) translate3d(0,-25px,0)",
+    }
+  ),
+});
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i001",
+//     {
+//       x: "50%",
+//       y: "0%",
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-100%",
+//       y: "0%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i002",
+//     {
+//       x: "200%",
+//       y: "0%",
+//       duration: 2,
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-100%",
+//       y: "0%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i003",
+//     {
+//       x: "100%",
+//       y: "0%",
+//       duration: 2,
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-350%",
+//       y: "0%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i004",
+//     {
+//       x: "100%",
+//       y: "130%",
+//       duration: 2,
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-150%",
+//       y: "130%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i005",
+//     {
+//       x: "100%",
+//       y: "0%",
+//       duration: 2,
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-100%",
+//       y: "0%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+// ScrollTrigger.create({
+//   trigger: ".section3_fixed",
+//   scrub: 1,
+//   animation: gsap.fromTo(
+//     ".i006",
+//     {
+//       x: "100%",
+//       y: "0%",
+//       duration: 2,
+//       backgroundSize: 0,
+//       // scale: 1,
+//     },
+//     {
+//       x: "-100%",
+//       y: "0%",
+//       ease: Linear.easeNone,
+//       // scale: 1.5,
+//       // objectFit: none,
+//     }
+//   ),
+// });
+
+//   섹션 4
+ScrollTrigger.create({
+  trigger: ".section4_fixed",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".section4_fixed",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "900px 18px",
+      transform: "translate(0,50px)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate3d(0,-25px,0)",
+    }
+  ),
+});
+
+let proxy = { skew: 0 },
+  skewSetter = gsap.quickSetter(".section4_cont>div", "skewY", "deg"), // fast
+  skewSetter2 = gsap.quickSetter(".section4_rev_icon_layout", "skewY", "deg"), // fast
+  clamp = gsap.utils.clamp(-10, 10);
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / -300);
+
+    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {
+        skew: 0,
+        duration: 0.8,
+        ease: "power3",
+        overwrite: true,
+        onUpdate: () => skewSetter(proxy.skew),
+      });
+    }
+  },
+});
+
+// make the right edge "stick" to the scroll bar. force3D: true improves performance
+gsap.set(".skewElem", { transformOrigin: "right center", force3D: true });
+
+//   섹션 5
+ScrollTrigger.create({
+  trigger: ".section5_fixed",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".section5_fixed",
+    {
+      scale: (1.3, 1.3),
+      opacity: 0,
+      transformOrigin: "900px 18px",
+      transform: "translate(-50%, -50%)",
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      transformOrigin: "50% 0%",
+      transform: "translate(-50%, -50%) translate3d(0,-25px,0)",
+    }
+  ),
+});
+
+//   섹션 5.5
+ScrollTrigger.create({
+  trigger: ".section5p5_fixed",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".section5p5_fixed",
+    {
+      opacity: 1,
+    },
+    {
+      opacity: 0,
+    }
+  ),
+});
+
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".i001", {
+    // transform: "translatex(-50%)",
+    x: "0%",
+  }),
+  animation: gsap.to(".i001", {
+    // transform: "translatex(-450%)",
+    x: "-650%",
+  }),
+});
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".i001 .item_desc", {
+    // transform: "translatex(-50%)",
+    x: "50%",
+  }),
+  animation: gsap.to(".i001 .item_desc", {
+    // transform: "translatex(-450%)",
+    x: "-700%",
+  }),
+});
+ScrollTrigger.create({
+  scrub: 1,
+  animation: gsap.from(".i002", {
+    // transform: "translatex(-50%)",
+    x: "0%",
+  }),
+  animation: gsap.to(".i002", {
+    // transform: "translatex(-250%)",
+    x: "-450%",
+  }),
+});
+
+// ScrollTrigger.create({
+//   trigger: "editorial_two",
+//   scrub: 0.1,
+//   animation: gsap.to(".section3_item", {
+//     duration: 10,
+//     scale: 1.5,
+//     // rotate: "20deg",
+//     // transform: "translate(-50%, -50%) translate3d(0,-25px,0)",
+//     // transformOrigin: "50% 0%",
+//   }),
+// });
+
+//   VUE_API MOVIE
+ScrollTrigger.create({
+  trigger: ".vr1",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec8_logo",
+    {
+      x: "-40%",
+      y: "-40%",
+      // scale: 1,
+    },
+    {
+      x: "30%",
+      y: "10%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".vr1",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec8_bg",
+    {
+      x: "50%",
+      y: "0%",
+      backgroundSize: 0,
+      // scale: 1,
+    },
+    {
+      x: "20%",
+      y: "0%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+      // objectFit: none,
+    }
+  ),
+});
+
+//   VUE_API TMDB
+ScrollTrigger.create({
+  trigger: ".vr2",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec10_logo",
+    {
+      x: "40%",
+      y: "40%",
+      // scale: 1,
+    },
+    {
+      x: "-30%",
+      y: "-10%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".vr2",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec10_bg",
+    {
+      x: "0%",
+      y: "0%",
+      backgroundSize: 0,
+      // scale: 1,
+    },
+    {
+      x: "20%",
+      y: "0%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+      // objectFit: none,
+    }
+  ),
+});
+
+//   VUE_API SITE
+ScrollTrigger.create({
+  trigger: ".vr3",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec11_logo",
+    {
+      x: "-40%",
+      y: "0%",
+      // scale: 1,
+    },
+    {
+      x: "30%",
+      y: "0%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+    }
+  ),
+});
+ScrollTrigger.create({
+  trigger: ".vr3",
+  scrub: 1,
+  animation: gsap.fromTo(
+    ".sec11_bg",
+    {
+      x: "50%",
+      y: "0%",
+      backgroundSize: 0,
+      // scale: 1,
+    },
+    {
+      x: "20%",
+      y: "0%",
+      ease: Linear.easeNone,
+      scale: 1.2,
+      // objectFit: none,
+    }
+  ),
+});
